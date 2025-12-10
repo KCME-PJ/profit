6f4d90199979e1419da72ebdd283e6b804ec2d65
KCME-PJ <ken-fujita@kcme.jp>
2025-12-10 11:59:26 +0900

    navbarのボタン修正

--- analysis.php
+++ analysis.php
@@ -72,10 +72,90 @@
     <nav class="navbar navbar-expand-lg bg-primary p-0" data-bs-theme="dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="./index.html">採算表</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item"><a class="nav-link active" href="./analysis.php">詳細集計</a></li>
-                    <li class="nav-item"><a class="nav-link" href="./index.html">ダッシュボードへ戻る</a></li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            CP
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="./cp/cp.php">CP計画</a></li>
+                            <li><a class="dropdown-item" href="./cp/cp_edit.php">CP編集</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            見通し
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="./forecast/forecast_edit.php">見通し編集</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            予定
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="./plan/plan_edit.php">予定編集</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            月末見込み
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="./outlook/outlook_edit.php">月末見込み編集</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            概算
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="./result/result_edit.php">概算実績編集</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            勘定科目設定
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="./account/account.php">勘定科目登録</a></li>
+                            <li><a class="dropdown-item" href="./account/account_list.php">勘定科目リスト</a></li>
+                            <li><a class="dropdown-item" href="./details/detail.php">詳細登録</a></li>
+                            <li><a class="dropdown-item" href="./details/detail_list.php">詳細リスト</a></li>
+                            <li><a class="dropdown-item" href="./offices/office.php">係登録</a></li>
+                            <li><a class="dropdown-item" href="./offices/office_list.php">係リスト</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div class="navbar-nav ms-auto">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            <i class="bi bi-person-fill"></i>&nbsp;
+                            user name さん
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li><a class="dropdown-item" href="#">Logout</a></li>
+                        </ul>
+                    </li>
                 </ul>
             </div>
         </div>
